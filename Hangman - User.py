a408153a65ad082fffa211eb0cfd59d6477818ab
Curtis Toupin <ctoupin@armstrongmonitoring.com>
2020-09-12 03:31:41 -0400

    Adding comments for code suggestions and next assignment

--- Hangman - User.py
+++ Hangman - User.py
@@ -7,21 +7,29 @@ Created on Wed May 13 19:07:15 2020
 
 import string
 
+#TODO CT 2020-09-12 next assignment: have the program ask the user to choose their own secret word but have it not show up on screen. 
+#Helpful googling term is "secure input python". Try checking out the getpass package
+#https://stackoverflow.com/questions/9202224/getting-command-line-password-input-in-python
 secret_word = 'lollipop'
 number_guesses = 6
 warnings = 3
 letters_guessed = []
 vowels = ["a","e","i","o","u"]
 
+#TODO CT 2020-09-12 a word is guessed if each of the unique letters in the word have been guessed
+#You can retrieve the unique letters from your secret word and letters guessed using the set() function
+#Then if the unique letters in secret_word is a subset of the unique letters in letters_guessed, you know the word has been guesed.
+#You can use the inbuilt .issubset() method for this
+#return set(secret_word).issubset(set(letters_guessed))
 def is_word_guessed(secret_word,letters_guessed):
     letters_guessed = ''.join(letters_guessed)
     for i in secret_word:
         for j in letters_guessed:
             if i == j:
                 flag = True
-                break
-        if i != j:
-            flag = False
+                break   #CT 2020-09-12 This function will return true if at least one letter in letters_guessed is in the secret_word. is this intended? CT
+        if i != j: #CT 2020-09-12 indent here is too short, from here j will only ever be the last letter in letters_guessed because the loop over j has already finished 
+            flag = False 
             break
     return flag
     
@@ -30,25 +38,25 @@ def get_guessed_word(secret_word,letters_guessed):
     letters_guessed = ''.join(letters_guessed)
     alphabet = ''
     for i in secret_word:
-        for j in letters_guessed:
+        for j in letters_guessed: #CT 2020-09-12 this loop can be replaced by a check for "if i in letters_guessed:"
             if i == j:
                 alphabet += i
                 break
         else:
-            alphabet += " _ "
+            alphabet += " _ " #CT 2020-09-12 this will put two spaces between consecutive underscores, you might want to consider changing to " _" 
     return alphabet
     
 
 def get_available_letters(letters_guessed):
-    letters_remaining = list(string.ascii_lowercase)
-    for i in letters_guessed:
-        letters_remaining.remove(i)
+    letters_remaining = list(string.ascii_lowercase) #CT 2020-09-12 instead of doing this every time, consider keeping a single list of available letters, and when a letter is guessed used the .pop() method to pull it into the letters_guessed list
+    for i in letters_guessed: # Details on the .pop() method: https://www.geeksforgeeks.org/python-list-pop/ - the .pop() method removes a chosen member of a list and returns that member so that you could, for example, append it to antoher list
+        letters_remaining.remove(i) #i.e. letters_guessed.append(letters_remaining.pop('a')) will remove the letter 'a' from letters_remaining and add it to letters_guessed
     return(''.join(letters_remaining))
 
 
 def unique_letters(secret_word):
-    alphabet = list(string.ascii_lowercase)
-    for i in secret_word:
+    alphabet = list(string.ascii_lowercase) 
+    for i in secret_word: #CT 2020-09-12 len(set(secret_word)) returns the number of unique letters in secret word. You can use this and shorten this to return 26-len(set(secret_word))
         if i in alphabet:
             alphabet.remove(i)
     return 26-len(alphabet)
@@ -114,4 +122,4 @@ while number_guesses != 0:
         print("Available letters:",get_available_letters(letters_guessed))
         
 else:
-    print("Well. You officially suck at Hangman. The secret word was",secret_word)
+    print("Well. You officially suck at Hangman. The secret word was",secret_word) #rude :(
